ac4ab35ff5129dbfd9c5135e4054e0fcc33d31e3
Oleg Sofrygin <olegso@gmail.com>
2017-05-06 17:46:28 -0700

    faster conversion

--- R/hbgd_fittedTrajectory.R
+++ R/hbgd_fittedTrajectory.R
@@ -1,4 +1,3 @@
-
 who_centile2value <- function(x, p = 50, x_var = "agedays", y_var = "htcm",
   sex = "Female", data = NULL) {
 
@@ -75,6 +74,46 @@ who_zscore2value <- function(x, z = 0, y_var = "htcm", x_var = "agedays",
 who_zscore2htcm <- function(agedays, z = 0, sex = "Female") {
   who_zscore2value(agedays, z, x_var = "agedays", y_var = "htcm", sex = sex)
 }
+who_zscore2wtkg <- function(agedays, z = 0, sex = "Female") {
+  who_zscore2value(agedays, z, x_var = "agedays", y_var = "wtkg", sex = sex)
+}
+who_zscore2bmi <- function(agedays, z = 0, sex = "Female") {
+  who_zscore2value(agedays, z, x_var = "agedays", y_var = "bmi", sex = sex)
+}
+who_zscore2hcircm <- function(agedays, z = 0, sex = "Female") {
+  who_zscore2value(agedays, z, x_var = "agedays", y_var = "hcircm", sex = sex)
+}
+who_zscore2muaccm <- function(agedays, z = 0, sex = "Female") {
+  who_zscore2value(agedays, z, x_var = "agedays", y_var = "muaccm", sex = sex)
+}
+who_zscore2ssftmm <- function(agedays, z = 0, sex = "Female") {
+  who_zscore2value(agedays, z, x_var = "agedays", y_var = "ssftmm", sex = sex)
+}
+who_zscore2tsftmm <- function(agedays, z = 0, sex = "Female") {
+  who_zscore2value(agedays, z, x_var = "agedays", y_var = "tsftmm", sex = sex)
+}
+
+who_htcm2zscore <- function(agedays, htcm, sex = "Female") {
+  hbgd::who_value2zscore(agedays, htcm, x_var = "agedays", y_var = "htcm", sex = sex)
+}
+who_wtkg2zscore <- function(agedays, wtkg, sex = "Female") {
+  hbgd::who_value2zscore(agedays, wtkg, x_var = "agedays", y_var = "wtkg", sex = sex)
+}
+who_bmi2zscore <- function(agedays, bmi, sex = "Female") {
+  hbgd::who_value2zscore(agedays, bmi, x_var = "agedays", y_var = "bmi", sex = sex)
+}
+who_hcircm2zscore <- function(agedays, hcircm, sex = "Female") {
+  hbgd::who_value2zscore(agedays, hcircm, x_var = "agedays", y_var = "hcircm", sex = sex)
+}
+who_muaccm2zscore <- function(agedays, muaccm, sex = "Female") {
+  hbgd::who_value2zscore(agedays, muaccm, x_var = "agedays", y_var = "muaccm", sex = sex)
+}
+who_ssftmm2zscore <- function(agedays, ssftmm, sex = "Female") {
+  hbgd::who_value2zscore(agedays, ssftmm, x_var = "agedays", y_var = "ssftmm", sex = sex)
+}
+who_tsftmm2zscore <- function(agedays, tsftmm, sex = "Female") {
+  hbgd::who_value2zscore(agedays, tsftmm, x_var = "agedays", y_var = "tsftmm", sex = sex)
+}
 
 
 #' Wrapper to convert fit object into \code{hbgd}-compatible object
@@ -88,6 +127,53 @@ who_zscore2htcm <- function(agedays, z = 0, sex = "Female") {
 convert_to_hbgd <- function(fit_dat_all, data, sexvar = "sex", method = "default") {
     nodes <- attr(fit_dat_all, "nodes")
     ID <- nodes$IDnode
+    Ynode <- nodes$Ynode
+    if (ID != "subjid") stop("The subject identifier must be named 'subjid'")
+    # checkpoints = c(1, hbgd::months2days(1:24)),
+
+    if (Ynode %in% "haz") {
+      xy_pair_name = c("agedays","htcm")
+      fun_y_to_raw = who_zscore2htcm
+      fun_y_to_z = function(x, y, ...) return(y)
+
+    } else if (Ynode %in% "htcm") {
+      xy_pair_name = c("agedays","htcm")
+      fun_y_to_raw = function(x, y, ...) return(y)
+      fun_y_to_z = who_htcm2zscore
+
+    } else if (Ynode %in% "waz") {
+      xy_pair_name = c("agedays","wtkg")
+      fun_y_to_raw = who_zscore2wtkg
+      fun_y_to_z = function(x, y, ...) return(y)
+
+    } else if (Ynode %in% "wtkg") {
+      xy_pair_name = c("agedays","wtkg")
+      fun_y_to_raw = function(x, y, ...) return(y)
+      fun_y_to_z = who_wtkg2zscore
+
+    } else if (Ynode %in% "baz") {
+      xy_pair_name = c("agedays","bmi")
+      fun_y_to_raw = who_zscore2bmi
+      fun_y_to_z = function(x, y, ...) return(y)
+
+    } else if (Ynode %in% "bmi") {
+      xy_pair_name = c("agedays","bmi")
+      fun_y_to_raw = function(x, y, ...) return(y)
+      fun_y_to_z = who_bmi2zscore
+
+    } else if (Ynode %in% "hcaz") {
+      xy_pair_name = c("agedays","hcircm")
+      fun_y_to_raw = who_zscore2hcircm
+      fun_y_to_z = function(x, y, ...) return(y)
+
+    } else if (Ynode %in% "hcircm") {
+      xy_pair_name = c("agedays","hcircm")
+      fun_y_to_raw = function(x, y, ...) return(y)
+      fun_y_to_z = who_hcircm2zscore
+
+    } else {
+      stop("unrecognized outcome: " %+% Ynode)
+    }
 
     fit_dat_hbgd <- data %>%
       dplyr::distinct_(ID, sexvar) %>%
@@ -96,13 +182,115 @@ convert_to_hbgd <- function(fit_dat_all, data, sexvar = "sex", method = "default
     if (is.integerish(fit_dat_hbgd[["sex"]]))
       fit_dat_hbgd[["sex"]] <- as.integer(fit_dat_hbgd[["sex"]]) %>% dplyr::recode(`0` = "Female", `1` = "Male")
 
-    fit_dat_hbgd <- fit_dat_hbgd %>%
+    fit_dat_hbgd <-
+      fit_dat_hbgd %>%
       dplyr::left_join(fit_dat_all) %>%
-      tibble::as_tibble() %>%
-      plyr::mutate(fit = purrr::map2(fit, sex,
-          ~ add_cogs_persubj(fit_dat = .x, sex = .y, method = method)))
+      tibble::as_tibble()
 
-    fit_dat_hbgd
+    xy <-
+      fit_dat_hbgd %>%
+      tidyr::unnest(fit) %>%
+      dplyr::select_(ID, sexvar, "fit") %>%
+      tidyr::unnest(fit) %>%
+      dplyr::rename_("Y_modeled" = "y") %>%
+      dplyr::mutate(y = fun_y_to_raw(x, Y_modeled, sex = sex)) %>%
+      dplyr::mutate(z = fun_y_to_z(x, Y_modeled, sex = sex)) %>%
+      dplyr::mutate(yfit = fun_y_to_raw(x, preds, sex = sex)) %>%
+      dplyr::mutate(zfit = fun_y_to_z(x, preds, sex = sex)) %>%
+      dplyr::select_(ID, "x", "y", "z", "yfit", "zfit") %>%
+      tidyr::nest(-subjid, .key = xy)
+      #  %>%
+      # dplyr::mutate(xy = purrr::map(xy, ~.x[1]))
+      # dplyr::mutate(xy = purrr::map(xy, function(x) {browser(); unlist(x)}))
+    # xy[["xy"]]
+    # xy[1, "xy"][[1]][[1]]
+
+    ## the residuals of the fit
+    resid <-
+      xy %>%
+      tidyr::unnest(xy) %>%
+      dplyr::mutate(resid = y - yfit) %>%
+      dplyr::select(subjid, resid) %>%
+      tidyr::nest(-subjid, .key = resid)
+
+    fitgrid <-
+      fit_dat_hbgd %>%
+      tidyr::unnest(fit) %>%
+      dplyr::select_(ID, sexvar, "fitgrid") %>%
+      tidyr::unnest(fitgrid) %>%
+      dplyr::mutate(y = fun_y_to_raw(x, preds, sex = sex)) %>%
+      dplyr::mutate(z = fun_y_to_z(x, preds, sex = sex)) %>%
+      dplyr::select_(ID, "x", "y", "z") %>%
+      dplyr::mutate(dy = NA, dz = NA) %>%
+      tidyr::nest(-subjid, .key = fitgrid)
+    ## add derivative on original and z-score scale
+    # if (!is.null(fitgrid$y)) fitgrid$dy <- hbgd::grid_deriv(fitgrid$x, fitgrid$y)
+    # if (!is.null(fitgrid$z)) fitgrid$dz <- hbgd::grid_deriv(fitgrid$x, fitgrid$z)
+    # res[["fitgrid"]] <- fitgrid
+
+    holdout <-
+      fit_dat_hbgd %>%
+      tidyr::unnest(fit) %>%
+      dplyr::select_(ID, sexvar, "holdout") %>%
+      tidyr::unnest(holdout) %>%
+      dplyr::mutate(y = fun_y_to_raw(x, preds, sex = sex)) %>%
+      dplyr::mutate(z = fun_y_to_z(x, preds, sex = sex)) %>%
+      dplyr::select_(ID, "x", "y", "z") %>%
+      tidyr::nest(-subjid, .key = holdout)
+
+    checkpoint <-
+      fit_dat_hbgd %>%
+      tidyr::unnest(fit) %>%
+      dplyr::select_(ID, sexvar, "checkpoint") %>%
+      tidyr::unnest(checkpoint) %>%
+      dplyr::mutate(y = fun_y_to_raw(x, preds, sex = sex)) %>%
+      dplyr::mutate(z = fun_y_to_z(x, preds, sex = sex)) %>%
+      dplyr::select_(ID, "x", "y", "z") %>%
+      dplyr::mutate(zcat = NA) %>%
+      tidyr::nest(-subjid, .key = checkpoint)
+
+      # for (i in 1:nrow(checkpoint))
+      #   print(all.equal(checkpoint[["checkpoint"]][[i]], fit_dat_hbgd_slow[["fit"]][[i]][["checkpoint"]]))
+      # fit_dat_hbgd_slow[["fit"]][[1]][["xy"]]
+
+    # browser()
+
+    fit_dat_hbgd_fast <-
+      fit_dat_hbgd %>%
+      select(subjid, sex) %>%
+      dplyr::left_join(xy) %>%
+      dplyr::left_join(resid) %>%
+      dplyr::left_join(fitgrid) %>%
+      dplyr::left_join(holdout) %>%
+      dplyr::left_join(checkpoint) %>%
+      dplyr::mutate(zcat = NA, pars = NA, x_var = xy_pair_name[1], y_var = xy_pair_name[2], method = method) %>%
+      tidyr::nest(-subjid, .key= fit) %>%
+      dplyr::mutate(fit = purrr::map(fit, function(res) {
+        # browser();
+        res <- list(
+          xy = res[["xy"]][[1]], resid = res[["resid"]][[1]], holdout = res[["holdout"]][[1]], checkpoint = res[["checkpoint"]][[1]], zcat = res[["zcat"]],
+          pars = res[["pars"]], x_var = res[["x_var"]], y_var = res[["y_var"]], method = res[["method"]], sex = res[["sex"]]
+          )
+        class(res) <- "fittedTrajectory";
+        res}))
+
+    # fit_dat_hbgd[1:10, ][["fit"]]
+    # fit_dat_hbgd_fast[["fit"]]
+    fit_dat_hbgd_fast
+    # fit_dat_hbgd_fast[1, ][["fit"]]
+
+    # fit_dat_hbgd_slow <-
+    #   fit_dat_hbgd %>%
+    #   plyr::mutate(fit = purrr::map2(fit, sex,
+    #       ~ add_cogs_persubj(fit_dat = .x, sex = .y, method = method)))
+    # fit_dat_hbgd_slow
+    # fit_dat_hbgd_slow[1, ][["fit"]]
+
+  # browser()
+  # checkpoint[1, "checkpoint"][[1]][[1]]
+  # unlist(fit_dat_hbgd_fast[1, "holdout"][[1]])
+  # fit_dat_hbgd_fast[1, "fit"][[1]]
+  # fit_dat_hbgd_slow[1, "fit"][[1]]
 }
 
 #' Create a fittedTrajectory object with fits for a single subject
